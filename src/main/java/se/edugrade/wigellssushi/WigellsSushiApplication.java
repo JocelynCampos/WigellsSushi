0a7460dde74cc0480c6e103cf2fb93b118e728f5
Jocelyn <aracely.campos@hotmail.com>
2025-09-23 14:17:37 +0200

    initial commit

---
+++ src/main/java/se/edugrade/wigellssushi/WigellsSushiApplication.java
@@ -0,0 +1,13 @@
+package se.edugrade.wigellssushi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WigellsSushiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WigellsSushiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/edugrade/wigellssushi/WigellsSushiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.edugrade.wigellssushi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WigellsSushiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
