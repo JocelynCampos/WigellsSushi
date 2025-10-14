41880814e01896238a59f027b61ba8e99f82d4cc
Jocelyn <aracely.campos@hotmail.com>
2025-10-14 10:53:53 +0200

    Fixar application.properties

--- src/main/java/se/edugrade/wigellssushi/controllers/PingController.java
+++ src/main/java/se/edugrade/wigellssushi/controllers/PingController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/wigellsuhsi/test")
+@RequestMapping("/api/wigellsuhsi")
 public class PingController {
 
-    @GetMapping("/ping")
+    @GetMapping("/test")
     public String ping() {
         return "Ping";
     }
