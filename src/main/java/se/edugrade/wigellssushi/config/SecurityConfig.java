e463e7b0022c8e7b279bbbf3fc303e52c67607e3
Jocelyn <aracely.campos@hotmail.com>
2025-10-23 20:57:49 +0200

    tog bort prefix

--- src/main/java/se/edugrade/wigellssushi/config/SecurityConfig.java
+++ src/main/java/se/edugrade/wigellssushi/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
         http
                 .csrf( csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/wigellsushi/test",
+                        .requestMatchers("/test",
                                 "/api/wigellsushi/euro-to-sek").permitAll()
 
                         .requestMatchers("/api/wigellsushi/rooms",
