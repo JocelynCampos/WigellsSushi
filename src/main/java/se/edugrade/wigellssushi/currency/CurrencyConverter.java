64c8a6c1e8b8582712298c336ddc374675eb0b1c
Jocelyn <aracely.campos@hotmail.com>
2025-10-24 09:46:31 +0200

    tog bort oanvända klasser

--- src/main/java/se/edugrade/wigellssushi/currency/CurrencyConverter.java
+++
@@ -1,9 +0,0 @@
-/*package se.edugrade.wigellssushi.currency;
-
-import java.math.BigDecimal;
-
-public interface CurrencyConverter {
-    BigDecimal convert(BigDecimal amount, String fromCurrency, String toCurrency);
-
-    default BigDecimal toSek(BigDecimal);
-}**/
